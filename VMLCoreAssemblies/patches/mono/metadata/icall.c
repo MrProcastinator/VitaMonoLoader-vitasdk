8d2d07a054449595135f00c52c7b2cc22f20e1b8
MrProcastinator <mrprocastinator@tutanota.com>
2025-04-26 19:38:21 -0300

    Add patch for System.Environment.TickCount call

--- VMLCoreAssemblies/patches/mono/metadata/icall.c
+++ VMLCoreAssemblies/patches/mono/metadata/icall.c
@@ -33,8 +33,24 @@ vml_icall_System_Text_Encoding_InternalCodePage(gint32 *int_code_page)
     return mono_string_new (mono_domain_get (), cset);
 }
 
+/*
+    Original: ves_icall_System_Environment_get_TickCount
+    Problem: When calling this function, it tries to open the file /proc/uptime, which is not available in PSVita
+    This causes a read exception and to avoid this overhead, we need to get the value directly from the kernel
+    Solution: use SceRtc functions to skip the file read
+*/
+static gint32
+ves_icall_System_Environment_get_TickCount (void)
+{
+    SceRtcTick tick;
+    sceRtcGetCurrentTick(&tick);
+    return (gint32) (tick.tick & 0xffffffff);
+}
+
+
 void VMLICallApplyPatches()
 {
     SCE_DBG_LOG_TRACE("Patching System.Text.Encoding::InternalCodePage...");
     mono_add_internal_call("System.Text.Encoding::InternalCodePage", vml_icall_System_Text_Encoding_InternalCodePage);
+    mono_add_internal_call("System.Environment::get_TickCount", ves_icall_System_Environment_get_TickCount);
 }
