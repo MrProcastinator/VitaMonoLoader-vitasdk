1c2a07e7d8f196841de6f0394dfdb76735985fe2
MrProcastinator <mrprocastinator@tutanota.com>
2025-09-29 12:44:29 -0300

    Add VML_OFFSET_ARRAY macro for misaligned IntPtr parameters

--- headers/VML/VMLTools.h
+++ headers/VML/VMLTools.h
@@ -2,9 +2,17 @@
 #define _VML_TOOLS_H_
 
 /*
+    Problem: sometimes passing pointer to array of structures are passed misaligned
+    Solution: use a macro to realign the array according to the situation
+*/
+
+#define VML_OFFSET_ARRAY(type, var, offset) ((type*)(((unsigned char*)(var))+offset))
+
+/*
     Problem: when marshalling arrays inside internal call parameters, there's a section of 16 bytes which is included before the actual data, making arrays unusable without offset adjustment
     Solution: use a macro to slip 16 bytes data to pass an array's data
 */
-#define VML_MARSHAL_ARRAY(type, var) ((type*)(((unsigned char*)(var))+16))
+
+#define VML_MARSHAL_ARRAY(type, var) VML_OFFSET_ARRAY(type, var, 16)
 
 #endif
